6e88371ce4d65058d0babd5de60cf23b435b4ee3
Mohamed BEN EL MOSTAPHA <benelmed36@gmail.com>
2024-10-30 15:01:50 +0100

    feat: define shell header and main .c file
    - Define the main global variables needed for the overall shell,
    as well as the functions.
    - Define the main source file that initializes the variables and
    implements the functions.

---
+++ fsh.c
@@ -0,0 +1,43 @@
+#include "include/fsh.h"
+#include "include/commands.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <readline/readline.h>
+#include <readline/history.h>
+
+// global variables' declaration
+char PREV_WORKING_DIR[PATH_MAX];
+char HOME[PATH_MAX];
+int PREV_RETURN_VALUE;
+char CWD[PATH_MAX];
+
+char *construct_prompt() {
+  char *prompt = malloc((strlen(CWD)+6)*sizeof(char));
+  snprintf(prompt, strlen(CWD)+6,"[%d]%s$ ",PREV_RETURN_VALUE, CWD); // FIXME : Should handle coloring and cutting the string if too long
+  return prompt;
+}
+
+
+int main(int argc, char* argv[], char *envp[]) {
+  char *ligne, *prompt;
+
+  // initialize global variable
+  strcpy(CWD, getcwd(NULL, 0));
+  strcpy(PREV_WORKING_DIR, CWD);
+
+  // extract HOME from env variables
+  while(strncmp(*envp, "HOME=",5) != 0) {
+    envp++;
+  }
+
+  strcpy(HOME, *(envp)+5);
+
+  while(1) {
+    prompt = construct_prompt(); // FIXME : Should not be reconstructed every time
+    ligne = readline(prompt);
+    add_history(ligne);
+    PREV_RETURN_VALUE = exec_simple_cmd(ligne);
+  }
+  return EXIT_SUCCESS;
+}
