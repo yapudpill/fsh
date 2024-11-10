6e5349398c512592f1f4b19ce3054cfeb62ffe56
Anthony Fernandes <anthony.fernandes@etu.u-paris.fr>
2024-11-10 17:07:15 +0100

    fix: forgot to change fsh.h

--- include/fsh.h
+++ include/fsh.h
@@ -7,7 +7,7 @@ extern int PREV_RETURN_VALUE;
 extern char *HOME;
 extern char CWD[PATH_MAX];
 
-extern char *construct_prompt();
+extern void update_prompt();
 extern int init_env_vars();
 extern int init_wd_vars();
 
